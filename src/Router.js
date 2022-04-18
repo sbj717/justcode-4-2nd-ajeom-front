2e1e1cc4fb33b758ccd53703858d496109930498
alicia-mkkim <alicia.mkkim@gmail.com>
2022-04-18 17:12:28 +0900

    [ADD]: first settings - add footer,nav

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,15 @@
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
+import Nav from './pages/components/Nav/Nav';
+import Footer from './pages/components/Footer/Footer';
 function Router() {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="" element={<Main />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
