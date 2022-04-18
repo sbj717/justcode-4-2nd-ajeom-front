733be3a7c525df17d0257474d9f0af603525cb21
alicia-mkkim <alicia.mkkim@gmail.com>
2022-04-18 17:07:59 +0900

    [ADD]: first settings

---
+++ src/Router.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './pages/Main/Main';
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
