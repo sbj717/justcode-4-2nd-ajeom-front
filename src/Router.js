0e62c732c14d3c161950706f05623c5aa5a50a0b
alicia-mkkim <alicia.mkkim@gmail.com>
2022-04-20 14:38:53 +0900

    [ADD]: 일반 유저 프로필 레이아웃 완성

--- src/Router.js
+++ src/Router.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
 import Nav from './pages/components/Nav/Nav';
 import Footer from './pages/components/Footer/Footer';
+import Profile from './pages/Profile/Profile';
 function Router() {
   return (
     <BrowserRouter>
       <Nav />
       <Routes>
         <Route path="" element={<Main />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
       <Footer />
     </BrowserRouter>
