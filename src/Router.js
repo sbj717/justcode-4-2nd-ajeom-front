6e21977772e1fa571a09fe3f8693f7937d9f35e2
HANSOL JEONG <losnah24@gmail.com>
2022-04-21 16:05:08 +0900

    Modify: Router 에서 Nav, Footer 제거

--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
-import Nav from './pages/components/Nav/Nav';
-import Footer from './pages/components/Footer/Footer';
 function Router() {
   return (
     <BrowserRouter>
-      <Nav />
       <Routes>
         <Route path="" element={<Main />} />
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 }
