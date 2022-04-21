9b029fc4acbcaae4f98886af43144cd65b8ad0ae
samuelseong <sbj717@gmail.com>
2022-04-21 21:20:13 +0900

    Resolve Conflict

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import BrunchbookTop from './pages/Brunchbook/BrunchbookTop';
 import Main from './pages/Main/Main';
+import Nav from './pages/components/Nav/Nav';
+import Footer from './pages/components/Footer/Footer';
 
 function Router() {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
-        <Route path="/main" element={<Main />} />
-        <Route path="" element={<BrunchbookTop />} />
+        <Route path="" element={<Main />} />
+        <Route path="/book" element={<BrunchbookTop />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
