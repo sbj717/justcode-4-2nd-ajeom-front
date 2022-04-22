b5d43a32dfc3bc149af1f9f63773f89aef99a269
alicia-mkkim <alicia.mkkim@gmail.com>
2022-04-22 14:56:49 +0900

    [modify] : profile 수정

--- src/pages/Profile/WriterProfile.js
+++ src/pages/Profile/WriterProfile.js
@@ -106,22 +106,26 @@ function WriterProfile() {
         }
       >
         <div className={styles.container}>
-          <div className={styles.bookContainer}>
-            <div className={styles.book}>
-              <span className={styles.bookLine} />
-              <div className={styles.titleBox}>보통의 서비스기획 PO 이야기</div>
-              <div className={styles.imgBox}>
-                <img
-                  src="https://images.unsplash.com/photo-1546874177-9e664107314e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80"
-                  alt="collection"
-                />
+          {[1, 2, 3, 4].map(li => (
+            <div className={styles.bookContainer}>
+              <div className={styles.book}>
+                <span className={styles.bookLine} />
+                <div className={styles.titleBox}>
+                  보통의 서비스기획 PO 이야기
+                </div>
+                <div className={styles.imgBox}>
+                  <img
+                    src="https://images.unsplash.com/photo-1546874177-9e664107314e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80"
+                    alt="collection"
+                  />
+                </div>
+              </div>
+              <div className={styles.bottom}>
+                <div className={styles.brunch}>brunch book</div>
+                <span>보통의 서비스기획 PO 이야기</span>
               </div>
             </div>
-            <div className={styles.bottom}>
-              <div className={styles.brunch}>brunch book</div>
-              <span>보통의 서비스기획 PO 이야기</span>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     );
