8bfa22e63a182c475fe4af9843afd0971f5457c0
nsoarim <99406537+nsoarim@users.noreply.github.com>
2022-04-28 15:58:24 +0900

    M: 작가 추천 클릭시 네비게이트

--- src/pages/Main/RecommendedWriter/RecommendedWriter.js
+++ src/pages/Main/RecommendedWriter/RecommendedWriter.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import styled, { css } from 'styled-components';
-
+import { useNavigate } from 'react-router-dom';
 function RecommendedWriter() {
   const [writerList, setWriterList] = useState([]);
-
+  const navigate = useNavigate();
   useEffect(() => {
     fetch('http://localhost:8000/user/authorList', {
       method: 'GET',
@@ -22,14 +22,20 @@ function RecommendedWriter() {
       <BoardTitle>AJEOM WRITERS</BoardTitle>
       <BoardDesc>아점 추천 작가</BoardDesc>
       <Board>
-        {writerListLimit.map(i => (
-          <Card key={i.id}>
-            <CardButton />
-            <ImageBox imageUrl={i.profile_img_url} />
-            <CardTitle>{i.nickname}</CardTitle>
-            <CardBody>{i.description}</CardBody>
-          </Card>
-        ))}
+        {writerListLimit.map((i, c) =>
+          c < 6 ? (
+            <Card key={i.id}>
+              <CardButton
+                onClick={() => {
+                  navigate(`/profile/${i.id}`);
+                }}
+              />
+              <ImageBox imageUrl={i.profile_img_url} />
+              <CardTitle>{i.nickname}</CardTitle>
+              <CardBody>{i.description}</CardBody>
+            </Card>
+          ) : null
+        )}
       </Board>
     </RecommendedWriterWrapper>
   );
